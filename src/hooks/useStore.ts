179522c129642b3a1e9377367d1cfaacf464306c
uttk <uttk8128@gmail.com>
2020-07-05 17:05:23 +0000

    Delete Class files

--- src/hooks/useStore.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { DefaultTrelaContext } from "../utils/context";
-import { Store, ApisBase } from "../types";
-
-export const useStore = <S, A extends ApisBase>(): Store<S, A> => {
-  const { store } = useContext(DefaultTrelaContext);
-
-  return store;
-};
