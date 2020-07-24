be9a44568cc6aacc9c85470a2c80fcdca24a1a46
uttk <uttk8128@gmail.com>
2020-07-24 21:16:54 +0000

    Create the createContextValue function

---
+++ src/util/createContextValue.ts
@@ -0,0 +1,35 @@
+import { StoreClass } from "../elements/store";
+import { FlowManagerClass } from "../elements/flowManager";
+import { DependencyManagerClass } from "../elements/dependencyManager";
+import { ApisBase, ContextOptions, TrelaContextValue, Setup } from "../type";
+
+export const createContextValue = <S, A extends ApisBase>(
+  options: ContextOptions<S, A>
+): TrelaContextValue<S, A> => {
+  const store = new StoreClass(options);
+  const dependencyMg = new DependencyManagerClass();
+
+  const setup: Setup<S, A> = (flow, dep) => {
+    const flowId = flow.id;
+
+    if (dep.hasFlowId(flowId)) return;
+
+    dep.bookUpdate(flowId);
+
+    flow.addEventCallback("started", () => {
+      if (dep.didMount) {
+        dep.updateComponentView();
+      }
+    });
+
+    flow.addEventCallback("finished", () => {
+      dependencyMg.tryUpdateView(flow.id);
+    });
+  };
+
+  return {
+    store,
+    dependencyMg,
+    flowMg: new FlowManagerClass(store, setup),
+  };
+};
