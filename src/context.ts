57ff99b11dc9ddb1f545b239280088aa06338641
uttk <uttk8128@gmail.com>
2020-07-25 16:15:54 +0000

    Delete elements from context.ts

--- src/context.ts
+++ src/context.ts
@@ -2,10 +2,8 @@ import { createContext } from "react";
 import { TrelaContextValue } from "./type";
 
 export const defaultContextValue: TrelaContextValue<any, any> = {
-  apis: {} as any,
   store: {} as any,
   flowMg: {} as any,
-  affecters: {} as any,
   dependencyMg: {} as any,
   isDefault: true,
 };
