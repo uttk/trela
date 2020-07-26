96d7ba10bdc38da2f85f526041821803619b785c
uttk <uttk8128@gmail.com>
2020-07-26 18:45:12 +0000

    Fix bugs of createSeriesRequest Create createSeriesRequest Tests

--- src/util/createSeriesRequest.ts
+++ src/util/createSeriesRequest.ts
@@ -14,35 +14,16 @@ export const createSeriesRequest = <S, A extends ApisBase>(
       removeCallbacks.forEach((f) => f());
       removeCallbacks.clear();
 
-      if (baseFlow.status !== "started") return;
       if (index >= len) return baseFlow.complete();
 
       const flow = flowList[index++];
+      const errorHandle = () => baseFlow.error(flow.currentError);
 
-      switch (flow.status) {
-        case "finished":
-          affect();
-          break;
+      removeCallbacks.add(flow.addEventCallback("finished", affect));
+      removeCallbacks.add(flow.addEventCallback("error", errorHandle));
+      removeCallbacks.add(flow.addEventCallback("cancel", baseFlow.cancel));
 
-        case "cancel":
-          baseFlow.cancel();
-          break;
-
-        case "error":
-          baseFlow.error(flow.currentError);
-          break;
-
-        case "started": {
-          const errorHandle = () => baseFlow.error(flow.currentError);
-
-          removeCallbacks.add(flow.addEventCallback("finished", affect));
-          removeCallbacks.add(flow.addEventCallback("error", errorHandle));
-          removeCallbacks.add(flow.addEventCallback("cancel", flow.cancel));
-
-          flow.start();
-          break;
-        }
-      }
+      flow.start();
     };
 
     affect();
---
+++ test/util/createSeriesRequest.spec.ts
@@ -0,0 +1,104 @@
+import { createSeriesRequest } from "@/util/createSeriesRequest";
+import { StoreClass } from "@/elements/store";
+import { FlowClass } from "@/elements/flow";
+import { Store } from "@/type";
+
+describe("createSeriesRequest", () => {
+  const initState = { test: "" };
+
+  type StateType = typeof initState;
+  type Apis = { test: () => Promise<string> };
+
+  const createRequest = (payload: FlowClass<StateType, Apis>[]) => {
+    return createSeriesRequest(payload);
+  };
+
+  let apis: Apis;
+  let apiMock: jest.Mock<Promise<string>, []>;
+  let store: Store<StateType, Apis>;
+
+  beforeEach(() => {
+    apiMock = jest.fn(async () => "test");
+    apis = { test: apiMock };
+    store = new StoreClass({ apis, reducer: (s) => s, initState });
+  });
+
+  test("Return a request function for parallel", () => {
+    const request = createRequest([]);
+
+    expect(request).toBeInstanceOf(Function);
+  });
+
+  test("Can execute children flows' the start function in sequence", () => {
+    const children = [1, 2, 3].map((id) => {
+      const flow = new FlowClass(id, store, (flow) => flow.complete());
+
+      flow.start = jest.fn(flow.start);
+
+      return flow;
+    });
+    const seriesFlow = new FlowClass(1111, store, createRequest(children));
+
+    seriesFlow.start();
+    expect(seriesFlow.status).toEqual("finished");
+
+    children.forEach((flow) => {
+      expect(flow.start).toBeCalledTimes(1);
+    });
+  });
+
+  test("Can execute complete function when all children flows complete", () => {
+    const childComplete: Array<() => void> = [];
+    const children = [1, 2, 3].map((id) => {
+      return new FlowClass(id, store, (flow) =>
+        childComplete.push(flow.complete)
+      );
+    });
+    const seriesFlow = new FlowClass(1111, store, createRequest(children));
+    const completeMock = jest.fn();
+
+    seriesFlow.complete = completeMock;
+
+    seriesFlow.start();
+    childComplete.shift()?.(); // execute a complete of first flow
+    expect(completeMock).not.toBeCalled();
+
+    childComplete.shift()?.(); // execute a complete of second flow
+    expect(completeMock).not.toBeCalled();
+
+    childComplete.shift()?.(); // execute a complete of third flow
+    expect(completeMock).toBeCalledTimes(1);
+  });
+
+  test("Can execute error function when children flows throw an error", () => {
+    const children = [1, 2, 3].map((id) => {
+      return new FlowClass(id, store, (flow) => {
+        if (id === 2) flow.error(new Error());
+        else flow.complete();
+      });
+    });
+    const seriesFlow = new FlowClass(2, store, createRequest(children));
+    const errorMock = jest.fn();
+
+    seriesFlow.error = errorMock;
+
+    seriesFlow.start();
+    expect(errorMock).toBeCalledTimes(1);
+  });
+
+  test("Can execute cancel function when children flows cancel", () => {
+    const children = [1, 2, 3].map((id) => {
+      return new FlowClass(id, store, (flow) => {
+        if (id === 2) flow.cancel();
+        else flow.complete();
+      });
+    });
+    const seriesFlow = new FlowClass(2, store, createRequest(children));
+    const cancelMock = jest.fn();
+
+    seriesFlow.cancel = cancelMock;
+
+    seriesFlow.start();
+    expect(cancelMock).toBeCalledTimes(1);
+  });
+});
