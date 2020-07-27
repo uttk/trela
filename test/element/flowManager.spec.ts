4ee33dc10b125695b64152e8bb5f88e31add7421
uttk <uttk8128@gmail.com>
2020-07-27 22:05:39 +0000

    Removed tests for some functions in FlowManager

--- test/element/flowManager.spec.ts
+++ test/element/flowManager.spec.ts
@@ -1,7 +1,6 @@
+import { FlowClass } from "@/elements/flow";
 import { FlowManagerClass } from "@/elements/flowManager";
 import { StoreClass } from "@/elements/store";
-import { DependencyClass } from "@/elements/dependency";
-import { FlowApi } from "@/index";
 
 describe("Flow Manager Class", () => {
   const initState = { test: "test" };
@@ -15,7 +14,7 @@ describe("Flow Manager Class", () => {
 
   beforeEach(() => {
     store = new StoreClass({ apis, initState, reducer: (s) => s });
-    flowMg = new FlowManagerClass(store, () => void 0);
+    flowMg = new FlowManagerClass(store);
   });
 
   describe("createId", () => {
@@ -31,9 +30,11 @@ describe("Flow Manager Class", () => {
 
   describe("returnFlowCache", () => {
     test("Return a flow when has not yet register the flow", () => {
-      const factory = () => flowMg.createFlow("test", []);
-      const flow = flowMg["returnFlowCache"](1, factory);
-      const flow2 = flowMg["returnFlowCache"](1, factory);
+      const baseFlow = new FlowClass<StateType, Apis>(1, store, () => {});
+      const sameIdFlow = new FlowClass<StateType, Apis>(1, store, () => {});
+
+      const flow = flowMg["returnFlowCache"](baseFlow);
+      const flow2 = flowMg["returnFlowCache"](sameIdFlow);
 
       expect(flow).toEqual(flow2);
     });
@@ -41,74 +42,18 @@ describe("Flow Manager Class", () => {
 
   describe("getFlow", () => {
     test("Return a flow that has the passed flowId", () => {
-      const flow = flowMg.createFlow("test", []);
+      const flow = flowMg.createFlow(1, () => {});
 
       expect(flowMg.getFlow(flow.id)).toEqual(flow);
-    });
-  });
-
-  describe("createSeriesFlow", () => {
-    test("Return a series flow", () => {
-      const childFlow = flowMg.createFlow("test", []);
-      const resultFlow = flowMg.createSeriesFlow([childFlow]);
-      const ids = [...flowMg["flowIds"].entries()];
-
-      ids.forEach(([key, id]) => {
-        if (resultFlow.id === id) {
-          expect(key).toContain("s:");
-        } else {
-          expect(childFlow.id).toEqual(id);
-        }
-      });
-    });
-  });
-
-  describe("createParallelFlow", () => {
-    test("Return a parallel flow", () => {
-      const childFlow = flowMg.createFlow("test", []);
-      const resultFlow = flowMg.createParallelFlow([childFlow]);
-      const ids = [...flowMg["flowIds"].entries()];
-
-      ids.forEach(([key, id]) => {
-        if (resultFlow.id === id) {
-          expect(key).toContain("p:");
-        } else {
-          expect(childFlow.id).toEqual(id);
-        }
-      });
     });
   });
 
   describe("createFlow", () => {
-    test("Returns the flow that executes the api", () => {
-      const flow = flowMg.createFlow("test", []);
-      const apiMock = jest.fn(apis.test);
+    test("Returns the flow", () => {
+      const flow = flowMg.createFlow(1, () => {});
 
-      apis.test = apiMock;
-
-      flow.start();
-      expect(apiMock).toBeCalledTimes(1);
+      expect(flow).toBeInstanceOf(FlowClass);
       expect(flowMg.getFlow(flow.id)).toEqual(flow);
-    });
-  });
-
-  describe("createFlowApi", () => {
-    test("Return a flow api object", () => {
-      const dependency = new DependencyClass(1, () => void 0);
-      const flow = flowMg.createFlow("test", []);
-      const flowApis = flowMg.createFlowApi(flow, dependency);
-      const keys = Object.keys(flowApis);
-      const requiredKeys: Array<keyof FlowApi<StateType>> = [
-        "cancel",
-        "error",
-        "forceStart",
-        "id",
-        "once",
-        "start",
-      ];
-
-      expect(flowApis).toBeInstanceOf(Object);
-      expect(keys.sort()).toStrictEqual(requiredKeys.sort());
     });
   });
 });
