68968588ca6ed55bc8ea4cafefddc09d331c5267
uttk <uttk8128@gmail.com>
2020-09-11 09:31:01 +0000

    Fixed to export trela elements

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
-export { useTrela } from "./hooks/useTrela";
-export { TrelaProvider } from "./components/Provider";
-export { createContextValue } from "./util/createContextValue";
+import { TrelaProvider } from "./components/Provider";
+import * as AppTypes from "./type";
+import { createTrelaContext } from "./util/createTrelaContext";
+
+namespace Trela {
+  export const Provider = TrelaProvider;
+
+  export const createContext = createTrelaContext;
+
+  /* eslint-disable prettier/prettier */
+
+  // trela
+  export type TrelaApi<A extends ApisBase> = AppTypes.TrelaApi<A>;
+  export type TrelaContext<A extends ApisBase> = AppTypes.TrelaContext<A>;
+  export type TrelaContextValue<A extends ApisBase> = AppTypes.TrelaContextValue<A>;
+  export type TrelaContextOptions<A extends ApisBase> = AppTypes.TrelaContextOptions<A>;
+
+  // utils
+  export type ApisBase = AppTypes.ApisBase;
+  export type ExcludeNull<T> = AppTypes.ExcludeNull<T>;
+  export type ResolvePromise<A extends ApisBase[string]> = AppTypes.ResolvePromise<A>;
+  export type CreateApiRequest<A extends ApisBase, AK extends keyof A> = AppTypes.CreateApiRequest<A, AK>;
+
+  /* eslint-enable prettier/prettier */
+}
+
 export {
-  Flow,
-  FlowApi,
-  Selector,
+  // trela
   TrelaApi,
-  ApisBase,
-  FlowStatus,
-  ContextOptions,
+  TrelaContext,
   TrelaContextValue,
-  CreateActionsType,
-  Reducer as TrelaReducer,
-} from "./type/index";
+  TrelaContextOptions,
+  // utility
+  ApisBase,
+  ExcludeNull,
+  ResolvePromise,
+  CreateApiRequest,
+} from "./type";
+
+export { TrelaProvider, createTrelaContext };
+
+export default Trela;
